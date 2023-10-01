140805411dd4bcc9d1e29ecbc1323ecf67332b4e
dujay <dujay971226@gmail.com>
2023-09-30 21:25:44 -0400

    Week 3 group Assignment Ver 0.7

--- src/main/java/CoreDB.java
+++ src/main/java/CoreDB.java
@@ -4,25 +4,23 @@ import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.json.JSONException;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CoreDB {
 
-    private static final String API_URL = "https://api.core.ac.uk/";
-    // load API_TOKEN from env variable.
     private static final String API_TOKEN = System.getenv("gauLY1I9Himz3kJOxdhpQEv52q0lP4BW");
 
-    public static String getApiToken() {
-        return API_TOKEN;
-    }
-
-    public Search findJournal(String content, String entityType) throws IOException {
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
+    public static String[] findJournal(String content, String entityType) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
         HttpUrl.Builder urlBuilder
-                = HttpUrl.parse("https://api.core.ac.uk/v3/search/" + entityType + "/").newBuilder();
+                = HttpUrl.parse("https://api.core.ac.uk/v3/search/" + entityType + "/")
+                .newBuilder();
         urlBuilder.addQueryParameter("api_key", "gauLY1I9Himz3kJOxdhpQEv52q0lP4BW");
         urlBuilder.addQueryParameter("q", content);
 
@@ -32,8 +30,19 @@ public class CoreDB {
                 .url(url)
                 .build();
 
+
         Response response = client.newCall(request).execute();
         System.out.println(response);
+        JSONObject responseBody = new JSONObject(response.body().string());
+
+
+        JSONArray result = responseBody.getJSONArray("results");
+        List<String> converter = new ArrayList<>();
+        for(int i = 0; i < result.length(); i++){
+            converter.add(result.getJSONObject(i).toString());
+        }
+        int size = converter.size();
+        return converter.toArray(new String[size]);
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+package main.java;
+
+import java.io.IOException;
+import java.lang.String;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String[] search = CoreDB.findJournal("cancer", "journals");
+        for (String item : search) {
+            System.out.println(item);
+        }
+    }
 }
+
